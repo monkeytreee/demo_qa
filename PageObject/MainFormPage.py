91604a506586b9e3e46f6a3779f246b667c762c0
makma4 <makma4>
2023-04-23 23:50:50 +0300

    fixed EXPECTED_MAIN_TITLE

--- PageObject/MainFormPage.py
+++ PageObject/MainFormPage.py
@@ -1,6 +1,6 @@
 class MainFormPage:
     # Константа с ожидаемым заголовком главной страницы
-    EXPECTED_MAIN_TITLE = "ToolsQA"
+    EXPECTED_MAIN_TITLE = 'DEMOQA'
 
     def __init__(self, driver):
         self.driver = driver
@@ -11,5 +11,5 @@ class MainFormPage:
 
     def verify_main_title(self, expected_main_title):
         # Проверяем, что заголовок главной страницы содержит ожидаемое значение
-        actual_title = self.driver.title
+        actual_title = self.get_main_title()
         assert expected_main_title in actual_title, f"Ошибка: ожидаемый заголовок '{self.EXPECTED_MAIN_TITLE}', фактический заголовок '{actual_title}'"
